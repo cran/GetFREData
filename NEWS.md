0711854b4e0d8f4775e98b1f6db5e6402067cb33
Marcelo Perlin <marceloperlin@gmail.com>
2022-06-13 10:00:02 +0000

    version 0.8.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,12 @@
+## Version 0.8.1 (2022-06-10)
+
+- fixed issue with ftp changing html (same problem as getdfpdata)
+
+## Version 0.8.0 (2022-04-07)
+
+- removed Date field in DESCRIPTION file
+- Fixed bug in get_info_companies
+
 ## Version 0.7 (2021-08-26)
 
 - Fixed issue in the aggregation of lists
--- tests/testthat/test-fredata.R
+++ tests/testthat/test-fredata.R
@@ -1,8 +1,10 @@
-test_that('info companies', {
+cache_folder <- tempdir()
+
+test_that('fetching info companies (no cache)', {
 
   skip_on_cran()
+  skip_if_offline()
 
-  cache_folder <- tempdir()
   my_info <- get_info_companies(cache_folder = cache_folder)
 
   expect_true(
@@ -11,12 +13,23 @@ test_that('info companies', {
 
 })
 
-
-test_that('main fct', {
+test_that('fetching info companies (with cache)', {
 
   skip_on_cran()
+  skip_if_offline()
 
-  cache_folder <- tempdir()
+  my_info <- get_info_companies(cache_folder = cache_folder)
+
+  expect_true(
+    (is.data.frame(my_info)) & (nrow(my_info) > 0)
+  )
+
+})
+
+test_that('fetch fre data', {
+
+  skip_on_cran()
+  skip_if_offline()
 
   my_id <- 19615
   l_fre <- get_fre_data(companies_cvm_codes = my_id,
@@ -26,7 +39,5 @@ test_that('main fct', {
   expect_true(
     (is.list(l_fre)) & (length(l_fre) > 0)
   )
-
-
 
 })
